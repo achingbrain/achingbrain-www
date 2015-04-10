c925c4bb73d98c49904f57ef8e23bc8f7f13d916
achingbrain <alex@achingbrain.net>
2015-04-10 11:00:35 +0100

    meatmon graph incoming

---
+++ public/js/meatmon.js
@@ -0,0 +1,71 @@
+console.info('Loading temperature data')
+
+async.parallel([
+  function(callback) {
+    $.getJSON('/meatmon/api/externalTemperatures', function(data) {
+      callback(null, data)
+    })
+  },
+  function(callback) {
+    $.getJSON('/meatmon/api/internalTemperatures', function(data) {
+      callback(null, data)
+    })
+  }
+], function(error, results) {
+  if(error) return console.error(error)
+
+  Highcharts.setOptions({
+    global: {
+      timezoneOffset: -60
+    }
+  });
+
+  $('#container').highcharts({
+    title: false,
+    chart: {
+      type: 'spline'
+    },
+    xAxis: {
+      type: 'datetime',
+      title: {
+        text: 'Time'
+      }
+    },
+    yAxis: {
+      title: {
+        text: 'Temperature (°C)'
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    tooltip: {
+      valueSuffix: '°C'
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    },
+    series: [{
+      name: 'External',
+      data: toSeries(results[0])
+    }, {
+      name: 'Internal',
+      data: toSeries(results[1])
+    }]
+  });
+})
+
+var toSeries = function(data) {
+  var output = []
+
+  data.forEach(function(datum) {
+    output.push([datum.date, datum.temp])
+  })
+
+  return output
+}
